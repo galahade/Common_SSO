f600f51f9ac6112e30e099f830cdafeb1cc14c21
galahade <galahade@sina.com>
2015-07-02 17:50:12 +0800

    add function

--- hateoas/src/test/java/bookmarks/BookmarkRestControllerTest.java
+++ hateoas/src/test/java/bookmarks/BookmarkRestControllerTest.java
@@ -99,6 +99,30 @@ public class BookmarkRestControllerTest {
 				.andExpect(jsonPath("$.description",is("A description")));
 	}
 	
+	@Test
+	public void readBookmarks() throws Exception {
+		mockMvc.perform(get("/" + userName + "/bookmarks"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(contentType))
+				.andExpect(jsonPath("$", hasSize(2)))
+				.andExpect(jsonPath("$[0].id", is(this.bookmarkList.get(0).getId().intValue())))
+				.andExpect(jsonPath("$[0].uri", is("http://bookmark.com/1/" + userName)))
+				.andExpect(jsonPath("$[0].description", is("A description")))
+                .andExpect(jsonPath("$[1].id", is(this.bookmarkList.get(1).getId().intValue())))
+                .andExpect(jsonPath("$[1].uri", is("http://bookmark.com/2/" + userName)))
+                .andExpect(jsonPath("$[1].description", is("A description")));
+	}
+	
+	@Test
+	public void createBookmark() throws Exception {
+		String bookmarkJson = json(new Bookmark(
+				this.account, "http://spring.io", "a bookmark to the best resource for Spring news and information"));
+		this.mockMvc.perform(post("/" + userName + "/bookmarks")
+                .contentType(contentType)
+                .content(bookmarkJson))
+                .andExpect(status().isCreated());
+	}
+	
 	@SuppressWarnings("unchecked")
 	protected String json(Object o) throws IOException {
 		MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
