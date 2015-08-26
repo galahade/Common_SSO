60a958fa4fe5c07f49922750add5219ebd1f8385
young_mac <young_mac@127.0.0.1>
2015-08-26 09:58:01 +0800

    Security config

--- security/src/main/java/com/yang/young/common/sso/config/Application.java
+++ security/src/main/java/com/yang/young/common/sso/config/Application.java
@@ -1,32 +1,20 @@
 package com.yang.young.common.sso.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.yang.young.common.sso.persistance.jpa.repository.CustomerRepository;
 
 @Configuration
 @EnableJpaRepositories(basePackages="com.yang.young.common.sso.persistance.jpa.repository")
 @EntityScan(basePackages="com.yang.young.common.sso.persistance.jpa.entity")
-@ComponentScan(basePackages={"com.yang.young.common.sso.persistance.jpa.service","com.yang.young.common.sso.rest"})
+@ComponentScan(basePackages={"com.yang.young.common.sso.persistance.jpa.service","com.yang.young.common.sso.rest","com.yang.young.common.sso.config"})
 @EnableAutoConfiguration
 public class Application {
 	
-	@Autowired
-	CustomerRepository service;
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ security/src/main/java/com/yang/young/common/sso/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,15 @@
+package com.yang.young.common.sso.config;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * No customizations of {@link AbstractSecurityWebApplicationInitializer} are necessary.
+ *
+ * @author Rob Winch
+ */
+@Order(2)
+public class MessageSecurityWebApplicationInitializer extends
+		AbstractSecurityWebApplicationInitializer {
+	
+}
